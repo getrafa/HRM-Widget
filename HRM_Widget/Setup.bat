f1dc6245b519398b2746b016263ef124a7ce6a7c
getrafa <getrafa@gmail.com>
2026-07-30 17:31:18 +0530

    Delete HRM_Widget directory

--- HRM_Widget/Setup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title HRM Live Widget Setup
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "setup.ps1"
-exit
--- HRM_Widget/Uninstall.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Uninstall HRM Live Widget
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "uninstall.ps1"
-exit
