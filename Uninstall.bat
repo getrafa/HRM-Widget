ca5071db26cbbed2e0a6a3d4373e0bba8e6454ae
getrafa <getrafa@gmail.com>
2026-09-02 16:55:38 +0530

    Delete Uninstall.bat

--- Uninstall.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if not "%1"=="h" (
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -FilePath '%0' -ArgumentList 'h' -WindowStyle Hidden"
-    exit
-)
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "uninstall.ps1"
-exit
